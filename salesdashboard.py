cea767d6d75c277fef7cfcd4429355e1ca27a70a
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2026-01-08 15:37:36 +0700

    Create salesdashboard.py

---
+++ salesdashboard.py
@@ -0,0 +1,56 @@
+from streamlit_option_menu import option_menu
+import streamlit as st
+
+# Page config
+st.set_page_config(page_title="BSI - Support Information", layout="wide")
+
+# NAVIGATION BAR (ATAS)
+st.title(" BSI Dashboard")
+st.info("Selamat Datang di Website BSI Dashboard!")
+selected = option_menu(
+    menu_title= None,
+    options=["Introduction","Dashboard Sales"],
+    icons=["calendar", "archive"],
+    orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#f0f2f6"},
+        "icon": {"color": "black", "font-size": "18px"},
+        "nav-link": {
+            "font-size": "16px",
+            "text-align": "center",
+            "margin": "0px",
+            "--hover-color": "#eee",
+        },
+        "nav-link-selected": {"background-color": "#ff4b4b", "color": "white"},
+    }
+)
+
+# SECTION CONTROL
+if selected == "Introduction":
+    st.warning("Perhatikan dengan cermat!")
+    st.markdown("""
+        <style>
+            .hover-zoom-box {
+                background-color: #28a745; /* Warna hijau */
+                color: white;
+                padding: 10px;
+                border-radius: 5px;
+                transition: transform 0.3s ease, background-color 0.3s ease;
+            }
+            .hover-zoom-box:hover {
+                transform: scale(1.05);
+                background-color: #218838; /* Warna hijau lebih gelap */
+            }
+        </style>
+                
+        <div class="hover-zoom-box">
+            - Website BSI Tools ini maintain by BSI Team.<br>
+            - BSI Tools digunakan untuk mempermudah dalam melakukan analisis, seperti POB, RNL, AO, Renpro, dan lainnya.<br>
+            - Diharapkan digunakan dengan baik.<br>
+            - Penanggung Jawab : <b> Nuril F. Amanah (Koordinator Business Support Information) </b>
+        </div>
+
+    """, unsafe_allow_html=True)
+elif selected == "Dashboard Sales":
+    import salesdashboard
+    salesdashboard.salesdashboard()
