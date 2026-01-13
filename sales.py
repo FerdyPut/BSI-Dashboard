954f40f4ac1b641396e58caea1488bf008ed5694
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2026-01-13 18:18:10 +0700

    Update sales.py

--- sales.py
+++ sales.py
@@ -1031,7 +1031,7 @@ def sales():
                 SUM("Historical Week: W5 {calendar.month_abbr[bulan_hist]}-{tahun_hist}") AS "W5 {calendar.month_abbr[bulan_hist]}-{tahun_hist}",
                 SUM("Total Historical Week") AS "Total Historical Week",
                 SUM(Target) AS Target,
-                NULL AS "Growth (%)
+                NULL AS "Growth (%)"
             FROM pivoted
         ),
 
